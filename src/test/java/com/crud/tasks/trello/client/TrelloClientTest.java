7b2563602cf0f5bee508fd9571d9e12d15e2f309
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-09 00:56:52 +0100

    Task 26.4

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -93,5 +93,19 @@ public class TrelloClientTest {
         assertEquals("http://test.com", newCard.getShortUrl());
     }
 
+    @Test
+    public void shouldReturnEmptyList() throws URISyntaxException {
+        //Given
+        URI uri = new URI("http://test.com/members/testUsername/boards?key=test&token=test&fields=name,id&lists=all");
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(null);
+
+        //When
+        List<TrelloBoardDto> returnedList = trelloClient.getTrelloBoards();
+
+        //Then
+        assertEquals(0, returnedList.size());
+    }
+
+
 
 }
