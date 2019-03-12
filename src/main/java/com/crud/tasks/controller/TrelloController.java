6d1c0e2fe3c79e793dc587c1e522a7a8853c9f3d
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-12 20:27:31 +0100

    re-configure for heroku

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/trello")
 public class TrelloController {
 
