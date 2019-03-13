bbcfa309dccbf1f4268a555b875c28e486ef01c3
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-13 13:14:43 +0100

    swagger configuration - limiting to controller package

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -35,7 +35,7 @@ public class CoreConfiguration implements WebMvcConfigurer { // Aby nasza aplika
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
