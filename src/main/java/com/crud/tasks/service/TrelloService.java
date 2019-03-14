6b1383eebc60139e3ff189f51cb6a1a9acc50171
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-14 13:18:51 +0100

    sending mail

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,45 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.mapper.CreatedTrelloCard;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TrelloService {
+    private static final String SUBJECT = "Tasks: New Trello card";
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+
+        Optional.ofNullable(newCard).ifPresent(e -> emailService.send(
+                                                          Mail.builder()
+                                                          .mailTo(adminConfig.getAdminMail())
+                                                          .subject(SUBJECT)
+                                                          .message("New card: " + trelloCardDto.getName() + " has been created on your account")
+                                                          .build() )
+                ); //opakowaliśmy newCard w metodę ofNullable klasy Optional, dzięki czemu możemy w łatwy sposób sprawdzić czy dana karta istnieje - za pomocą metody ifPresent, jako argument przyjmującej lambdę. Jeśli dana karta istnieje, lambda otrzyma ją jako parametr
+
+        return newCard;
+    }
+}
