c19798b3f37b5b19c45997bcb4992756c6d185cf
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-27 15:27:07 +0100

    test correction

--- src/test/java/com/crud/tasks/MockedRestTemplateTest.java
+++ src/test/java/com/crud/tasks/MockedRestTemplateTest.java
@@ -5,8 +5,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
