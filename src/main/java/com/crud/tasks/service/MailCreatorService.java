f175dc2181538ee79e15e8a02c68d862b048a7dd
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-18 14:00:58 +0200

    update

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,29 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+@Service
+public class MailCreatorService {
+    @Autowired
+    AdminConfig adminConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+        Context context = new Context(); // wewnątrz niej deklarujemy zmienne do widoku, który później przeprocesujemy za pomocą silnika szablonów wywołując templateEngine.process(). Nie stosujemy w tym wypadku modelu w którym rozszerzaliśmy mapę, ponieważ w tym wypadku Thymeleaf potrzebuje kontekstu dla swojego silnika widoku szablonu. Przyczyną tego rozwiązania jest obowiązek utworzenia w jednej metodzie widoku już ze wstawionymi zmiennymi do szablonu, przed wysłaniem maila.
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+
+        return templateEngine.process("mail/created-trello-card-mail", context); //Metoda process() przyjmuje dwa argumenty, jednym z nich jest ścieżka szablonu mail. Metoda wie już, że ma szukać szablonów w katalogu resources/templates, jeśli jednak umiejscowiliśmy nasz szablon "głębiej" w strukturze folderów, należy dodać taką informację w argumencie. Drugim argumentem jest Context, czyli obiekt przechowujący zmienne do widoku.
+    }
+}
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,11 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleEmailService {
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     @Autowired
     private JavaMailSender javaMailSender;
@@ -22,7 +26,7 @@ public class SimpleEmailService {
 
         try{
             SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Mail sent properly");
         } catch (MailException e) {
             LOGGER.error("Failed to send mail: ", e.getMessage(), e);
@@ -38,5 +42,15 @@ public class SimpleEmailService {
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) { //MIME to standard służący do wysyłania wiadomości e-mail. Dzięki temu standardowi, jesteśmy w stanie wysyłać obrazy, dźwięk i video.
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setCc(mail.getToCc());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 }
