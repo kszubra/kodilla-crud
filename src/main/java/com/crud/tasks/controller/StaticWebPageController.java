490bea836c33708c4ddd1ece93047f2e1d001f4a
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-18 12:49:02 +0200

    update

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -3,11 +3,14 @@ package com.crud.tasks.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller // działa identycznie jak adnotacja @RestController tylko nie zmienia odpowiedzi na JSON
 public class StaticWebPageController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Map<String, String> model) {
+        model.put("variable", "My Thymeleaf variable");
         return "index";
     }
 }
