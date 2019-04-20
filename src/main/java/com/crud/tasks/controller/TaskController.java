bb44d452f277698cccda3e347fe638e56528fef6
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-20 15:39:14 +0200

    controller refactor

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -12,34 +12,34 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @CrossOrigin(origins = "*")
 @RestController // Adnotacją oznaczamy klasę, którą chcemy "wystawić" dla świata — za jej pomocą tworzymy wejście do aplikacji z zewnętrznego świata
-@RequestMapping("/v1/task") // Dzięki tej operacji, nasz RestController będzie dostępny pod poniższym adresem: http://localhost:8080/v1/task
+@RequestMapping("/v1") // Dzięki tej operacji, nasz RestController będzie dostępny pod poniższym adresem: http://localhost:8080/v1/task
 public class TaskController {
     @Autowired
     private DbService service;
     @Autowired
     private TaskMapper taskMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks") // Dzięki operacjom, które przed chwilą wykonaliśmy, URL do pobrania wszystkich zadań to: http://localhost:8080/v1/task/getTasks
+    @RequestMapping(method = RequestMethod.GET, value = "tasks") // Dzięki operacjom, które przed chwilą wykonaliśmy, URL do pobrania wszystkich zadań to: http://localhost:8080/v1/task/getTasks
     public List<TaskDto> getTasks() {
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
-    @GetMapping("getTask/{id}")
+    @GetMapping("tasks/{id}")
     public TaskDto getTask(@PathVariable("id") Long taskId) {
         return taskMapper.mapToTaskDto(service.getTask(taskId));
     }
 
-    @DeleteMapping("deleteTask")
-    public void deleteTask(@RequestParam Long taskId) {
+    @DeleteMapping("tasks/{id}")
+    public void deleteTask(@PathVariable("id") Long taskId) {
         service.deleteTask(taskId);
     }
 
-    @PutMapping("updateTask")
+    @PutMapping("tasks")
     public TaskDto updateTask(@RequestBody TaskDto taskDto) {
         return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
     }
 
-    @PostMapping(value = "createTask", consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(value = "tasks", consumes = APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
         service.saveTask(taskMapper.mapToTask(taskDto));
     }
